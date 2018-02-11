b52e5e2f77401939534a5bda69410b2454540af7
chenqianwen <1433414364@qq.com>
2018-02-11 18:00:06 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	security-demo/pom.xml
    #	security-demo/src/main/resources/application.yml

--- security-core/src/main/java/com/example/security/core/authentication/FormAuthenticationConfig.java
+++ security-core/src/main/java/com/example/security/core/authentication/FormAuthenticationConfig.java
@@ -22,7 +22,8 @@ public class FormAuthenticationConfig {
 
     public void config(HttpSecurity http) throws Exception {
         http.formLogin()
-                .loginPage(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)// 指定的登录页面Url
+                // 未经过认证时，请求跳转的地址
+                .loginPage(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)
                 .loginProcessingUrl(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)//登录请求拦截的url,也就是form表单提交时指定的action
                 .successHandler(iAuthenticationSuccessHandler)//表单登陆之后调用自定义的 认证成功处理器
                 .failureHandler(iAuthenticationFailureHandler);//表单登陆之后调用自定义的 认证失败处理器
